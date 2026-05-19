4084cf2a01b0296d19e5ad92cf5463482148b07e
amineeelhani <elhaniaminee@gmail.com>
2026-05-19 17:22:50 +0200

    test

--- models/clip_finetuned_retrieval.py
+++ models/clip_finetuned_retrieval.py
@@ -88,7 +88,7 @@ for i, query_filename in enumerate(query_filenames):
 from utils.submit import evaluate_local
 
 ground_truth = {
-    "Brad1.png": ["Brad1_copy.png"]
+    "Brad1.png": ["Brad2.png","Brad3.jpg"]
 }
 print("Results:", results)
 print("Ground truth:", ground_truth)
