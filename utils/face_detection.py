d128b17e46886b2546949597d30591942f174aed
amineeelhani <elhaniaminee@gmail.com>
2026-05-19 16:52:37 +0200

    test no mtcnn

--- utils/face_detection.py
+++ utils/face_detection.py
@@ -5,7 +5,7 @@ def load_mtcnn(device):
     #return MTCNN(image_size=224, keep_all=False, device=device)
     return None
 def detect_face(img, mtcnn, preprocess):
-    face = mtcnn(img)
-    if face is not None:
-        return face
+    #face = mtcnn(img)
+    #if face is not None:
+        #return face
     return preprocess(img.convert("RGB"))
